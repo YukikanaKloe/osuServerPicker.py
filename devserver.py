57e3da5ddf01fe84c9c76d48279db1f1e5c1a219
Yukikana <98742915+yukikanakoe@users.noreply.github.com>
2022-12-31 01:32:14 +0700

    Add files via upload

---
+++ devserver.py
@@ -0,0 +1,3 @@
+tk.Button(root,text="Bancho",command = lambda:devserver("Bancho")).pack()
+tk.Button(root,text="Ripple",command = lambda:devserver("Ripple")).pack()
+tk.Button(root,text="Akatsuki",command = lambda:devserver("Akatsuki")).pack()
